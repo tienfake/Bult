176a209ad3974046f21fec233f0975a99d3e4563
nguyentien208 <nguyentien2082102@gmail.com>
2024-09-03 19:04:51 +0700

    ok

--- public/js/slide.js
+++ public/js/slide.js
@@ -1,47 +1,88 @@
-var swiper = new Swiper(".dichVuLK", {
-  slidesPerView: 4,
-  spaceBetween: 7,
-  centeredSlides: false,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    375: {
-      slidesPerView: 3.5,
-      spaceBetween: 7,
-    },
-    410: {
-      slidesPerView: 3.75,
-      spaceBetween: 3,
-    },
-    420: {
-      slidesPerView: 4,
-      spaceBetween: 7,
-    },
-  },
-});
+var GUI = {
+  slideService: () => {
+    if (document.querySelector(".dichVuLK")) {
+      var swiper = new Swiper(".dichVuLK", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        centeredSlides: false,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+          },
+          375: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
 
-var swiper = new Swiper(".slideBottom", {
-  spaceBetween: 0,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
+        },
+      });
+    }
   },
-  speed: 1000,
-});
 
-var swiper = new Swiper(".slideNews", {
-  slidesPerView: 2.3,
-  spaceBetween: 10,
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  slideBottom: () => {
+    if (document.querySelector(".slideBottom")) {
+      var swiper = new Swiper(".slideBottom", {
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        speed: 1000,
+      });
+    }
   },
+  slideNews: () => {
+    if (document.querySelector(".slideNews")) {
+      var swiper = new Swiper(".slideNews", {
+        slidesPerView: 2.3,
+        spaceBetween: 10,
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    }
+  },
+
+  slideServiceCT: () => {
+    if (document.querySelector(".dichVuCT")) {
+      var swiper = new Swiper(".dichVuCT", {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        centeredSlides: false,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+          },
+          375: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+          },
+
+        },
+      });
+    }
+  },
+  init: () => {
+    GUI.slideService();
+    GUI.slideBottom();
+    GUI.slideNews();
+    GUI.slideServiceCT();
+    
+  },
+};
+document.addEventListener("DOMContentLoaded", () => {
+  GUI.init();
 });
